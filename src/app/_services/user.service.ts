bcfac665388de83fa5c69b5a5db453304ebf8c3a
Agusdeluca96 <Agus@Delu-MacBook-Pro.local>
2019-03-31 20:46:08 -0300

    Modificado el metodo que devuelve las rutas a descubrir, agregandole los respectivos filtros de busqueda

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment'
 import { Observable } from 'rxjs';
 import { User, Ruta } from '../_models';
@@ -29,29 +29,40 @@ export class UserService {
 
     /** PUT: modify attr 'habilitado' from false to true of an user in the database */
     habilitar(user: User): Observable<User> {
-        const url = environment.api_url + 'usuario/'.concat(user.id.toString(), '/habilitar');
+        const url = environment.api_url + 'usuario/' + user.id.toString() + '/habilitar';
         return this.http.put<User>(url, httpOptions);
     }
 
     /** PUT: modify attr 'habilitado' from true to false of an user in the database */
     deshabilitar(user: User): Observable<User> {
-        const url = environment.api_url + 'usuario/'.concat(user.id.toString(), '/deshabilitar');
+        const url = environment.api_url + 'usuario/' + user.id.toString() + '/deshabilitar';
         return this.http.put<User>(url, httpOptions);
     }
 
     /** GET Rutas Agregadas by the User from the database */
     listAllRutasAgregadas(user: User): Observable<Ruta[]> {
-        return this.http.get<Ruta[]>(environment.api_url + 'usuario/'.concat(user.id.toString(), '/rutasAgregadas'));
+        return this.http.get<Ruta[]>(environment.api_url + 'usuario/' + user.id.toString() + '/rutasAgregadas');
     }
 
     /** GET Rutas Recorridas by the User from the database */
     listAllRutasRecorridas(user: User): Observable<Ruta[]> {
-        return this.http.get<Ruta[]>(environment.api_url + 'usuario/'.concat(user.id.toString(), '/rutasRecorridas'));
+        return this.http.get<Ruta[]>(environment.api_url + 'usuario/' + user.id.toString() + '/rutasRecorridas');
     }
 
     /** GET Rutas Descubrir by the User from the database */
-    listAllRutasDescubrir(user: User): Observable<Ruta[]> {
-        return this.http.get<Ruta[]>(environment.api_url + 'usuario/'.concat(user.id.toString(), '/rutasDescubrir'));
+    listAllRutasDescubrir(user: User, actividad: string, formato: string, dificultad: string): Observable<Ruta[]> {
+        let params = new HttpParams();
+        if (actividad != '') {
+            params = params.set('actividad', actividad);
+        }
+        if (formato != '') {
+            params = params.set('formato', formato);
+        }
+        if (dificultad != '') {
+            params = params.set('dificultad', dificultad);
+        }
+        console.log(params.get('actividad'));
+        return this.http.get<Ruta[]>(environment.api_url + 'usuario/' + user.id.toString() + '/rutasDescubrir', { params: params });
     }
 
     /** POST: add a new ruta to User known routes to the database */
